885bf8c7c8acf2ba6f708df31dd41bde4d843833
KC Wong <kc.wong@igsl-group.com>
2023-05-19 14:59:14 +0800

    - Added support for Jira user properties and group memberships. Both are now synchronized during merge.

--- ConfigMigration/src/main/java/com/igsl/configmigration/applicationuser/ApplicationUserDTO.java
+++ ConfigMigration/src/main/java/com/igsl/configmigration/applicationuser/ApplicationUserDTO.java
@@ -1,25 +1,78 @@
 package com.igsl.configmigration.applicationuser;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
+import org.apache.log4j.Logger;
+
+import com.atlassian.crowd.embedded.api.CrowdService;
 import com.atlassian.crowd.embedded.api.Directory;
-import com.atlassian.crowd.manager.directory.DirectoryManager;
+import com.atlassian.crowd.embedded.api.Group;
+import com.atlassian.crowd.embedded.api.User;
+import com.atlassian.jira.bc.user.GroupView;
+import com.atlassian.jira.bc.user.UserApplicationHelper;
 import com.atlassian.jira.component.ComponentAccessor;
 import com.atlassian.jira.user.ApplicationUser;
+import com.atlassian.jira.user.UserPropertyManager;
 import com.atlassian.jira.user.util.UserManager;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.igsl.configmigration.JiraConfigDTO;
 import com.igsl.configmigration.JiraConfigProperty;
 import com.igsl.configmigration.JiraConfigUtil;
+import com.igsl.configmigration.general.GeneralDTO;
+import com.igsl.configmigration.general.GeneralUtil;
+import com.igsl.configmigration.group.GroupDTO;
+import com.igsl.configmigration.group.GroupUtil;
+import com.opensymphony.module.propertyset.PropertySet;
 
 @JsonDeserialize(using = JsonDeserializer.None.class)
 public class ApplicationUserDTO extends JiraConfigDTO {
 
+	private static final Logger LOGGER = Logger.getLogger(ApplicationUserDTO.class);
 	private static final UserManager USER_MANAGER = ComponentAccessor.getUserManager();
+	private static final UserPropertyManager PROPERTY_MANAGER = ComponentAccessor.getUserPropertyManager();	
+	private static final CrowdService CROWD_SERVICE = ComponentAccessor.getCrowdService();
+	private static final UserApplicationHelper USER_APPLICATION_HELPER = ComponentAccessor.getComponent(UserApplicationHelper.class);
+	
+	/*
+	 * 	SQL for checking user property values
+		SELECT 
+			e.*, 
+			CASE e.PROPERTYTYPE
+				WHEN 1 THEN 'Boolean'
+				WHEN 2 THEN 'Int'
+				WHEN 3 THEN 'Long'
+				WHEN 4 THEN 'Double'
+				WHEN 5 THEN 'String 255'
+				WHEN 6 THEN 'String unlimited'
+				WHEN 7 THEN 'Date'
+				WHEN 8 THEN 'Object'
+				WHEN 9 THEN 'XML'
+				WHEN 10 THEN 'Data'
+			END as Type,
+			CASE e.PROPERTYTYPE
+				WHEN 1 OR 2 OR 3 THEN CONVERT(number.PROPERTYVALUE, VARCHAR)
+				WHEN 4 THEN CONVERT(decimal.PROPERTYVALUE, VARCHAR)
+				WHEN 5 THEN CONVERT(string.PROPERTYVALUE, VARCHAR)
+				WHEN 6 THEN CONVERT(text.PROPERTYVALUE, VARCHAR)
+				WHEN 7 THEN CONVERT(date.PROPERTYVALUE, VARCHAR)
+				WHEN 8 OR 9 OR 10 THEN CONVERT(data.PROPERTYVALUE, VARCHAR)
+			END as Value
+		FROM PROPERTYENTRY e 
+			LEFT JOIN PROPERTYNUMBER number ON number.ID = e.ID AND e.PROPERTYTYPE IN (1, 2, 3)
+			LEFT JOIN PROPERTYDECIMAL decimal on decimal.ID = e.ID and e.PROPERTYTYPE IN (4)
+			LEFT JOIN PROPERTYSTRING string on string.ID = e.ID and e.PROPERTYTYPE IN (5)
+			LEFT JOIN PROPERTYTEXT text on text.ID = e.ID and e.PROPERTYTYPE IN (6)
+			LEFT JOIN PROPERTYDATE date ON date.ID = e.ID and e.PROPERTYTYPE IN (7)
+			LEFT JOIN PROPERTYDATA data ON data.ID = e.ID AND e.PROPERTYTYPE IN (8, 9, 10)
+		WHERE e.ENTITY_NAME = 'ApplicationUser' AND e.ENTITY_ID LIKE '%';
+	 */
+	
 	// TODO Better way to identify internal directory?
 	private static final String JIRA_INTERNAL_DIRECTORY = "Jira Internal Directory";	
 	private Long id;
@@ -29,6 +82,8 @@ public class ApplicationUserDTO extends JiraConfigDTO {
 	private String emailAddress;
 	private String displayName;
 	private boolean jiraUser;	// If user is in Jira, i.e. not in external directory
+	private Map<String, GeneralDTO> properties;
+	private List<GroupDTO> groups;
 	
 	@Override
 	public void fromJiraObject(Object obj) throws Exception {
@@ -44,8 +99,43 @@ public class ApplicationUserDTO extends JiraConfigDTO {
 		if (dir != null) {
 			jiraUser = dir.getName().equals(JIRA_INTERNAL_DIRECTORY);
 		}
+		// Properties
+		this.properties = new HashMap<>();
+		PropertySet ps = PROPERTY_MANAGER.getPropertySet(o);
+		LOGGER.debug("User: " + this.name + " Properties count: " + ps.getKeys().size());
+		for (Object k : ps.getKeys()) {
+			String key = String.valueOf(k);
+			Object value = ps.getAsActualType(key);
+			LOGGER.debug("Property [" + key + "][" + value + "]" + ((value != null)? "(" + value.getClass().getCanonicalName() + ")" : ""));
+			GeneralDTO dto = new GeneralDTO();
+			dto.setJiraObject(value);
+			this.properties.put(key, dto);
+		}
+		// Group membership
+		// GroupManager is not used here because it seems to be caching information and has a delay in update
+		// CrowdService is used instead
+		this.groups = new ArrayList<>();
+		for (GroupView gv : USER_APPLICATION_HELPER.getUserGroups(o)) {
+			Group grp = CROWD_SERVICE.getGroup(gv.getName());
+			if (grp != null) {
+				User user = CROWD_SERVICE.getUser(o.getName());
+				if (CROWD_SERVICE.isUserDirectGroupMember(user, grp)) {
+					GroupDTO dto = new GroupDTO();
+					dto.setJiraObject(grp);
+					this.groups.add(dto);
+				}
+			}
+		}
 	}
 
+	@Override
+	public void setupRelatedObjects() {
+		for (GroupDTO dto : this.groups) {
+			this.addRelatedObject(dto);
+			dto.addReferencedObject(this);
+		}
+	}
+	
 	@Override
 	protected Map<String, JiraConfigProperty> getCustomConfigProperties() {
 		Map<String, JiraConfigProperty> r = new TreeMap<>();
@@ -54,6 +144,8 @@ public class ApplicationUserDTO extends JiraConfigDTO {
 		r.put("Email Address", new JiraConfigProperty(emailAddress));
 		r.put("Key", new JiraConfigProperty(key));
 		r.put("ID", new JiraConfigProperty(id));
+		r.put("Properties", new JiraConfigProperty(GeneralUtil.class, this.properties));
+		r.put("Groups", new JiraConfigProperty(GroupUtil.class, this.groups));
 		r.put("Jira Internal Directory User", new JiraConfigProperty(this.jiraUser));
 		return r;
 	}
@@ -148,6 +240,22 @@ public class ApplicationUserDTO extends JiraConfigDTO {
 
 	public void setJiraUser(boolean jiraUser) {
 		this.jiraUser = jiraUser;
+	}
+
+	public Map<String, GeneralDTO> getProperties() {
+		return properties;
+	}
+
+	public void setProperties(Map<String, GeneralDTO> properties) {
+		this.properties = properties;
+	}
+
+	public List<GroupDTO> getGroups() {
+		return groups;
+	}
+
+	public void setGroups(List<GroupDTO> groups) {
+		this.groups = groups;
 	}
 
 }
--- ConfigMigration/src/main/java/com/igsl/configmigration/applicationuser/ApplicationUserUtil.java
+++ ConfigMigration/src/main/java/com/igsl/configmigration/applicationuser/ApplicationUserUtil.java
@@ -1,5 +1,7 @@
 package com.igsl.configmigration.applicationuser;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.TreeMap;
@@ -7,6 +9,10 @@ import java.util.TreeMap;
 import org.apache.log4j.Logger;
 
 import com.atlassian.crowd.embedded.api.Group;
+import com.atlassian.jira.bc.JiraServiceContext;
+import com.atlassian.jira.bc.JiraServiceContextImpl;
+import com.atlassian.jira.bc.group.GroupRemoveChildMapper;
+import com.atlassian.jira.bc.group.GroupService;
 import com.atlassian.jira.bc.user.UserService;
 import com.atlassian.jira.bc.user.UserService.CreateUserRequest;
 import com.atlassian.jira.bc.user.UserService.CreateUserValidationResult;
@@ -14,17 +20,19 @@ import com.atlassian.jira.bc.user.UserService.UpdateUserValidationResult;
 import com.atlassian.jira.bc.user.search.UserSearchParams;
 import com.atlassian.jira.bc.user.search.UserSearchService;
 import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.security.groups.GroupManager;
 import com.atlassian.jira.user.ApplicationUser;
-import com.atlassian.jira.user.UserDetails;
+import com.atlassian.jira.user.UserPropertyManager;
 import com.atlassian.jira.user.util.UserManager;
-import com.atlassian.jira.user.util.UserUtil;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.igsl.configmigration.DTOStore;
 import com.igsl.configmigration.JiraConfigDTO;
 import com.igsl.configmigration.JiraConfigUtil;
 import com.igsl.configmigration.MergeResult;
+import com.igsl.configmigration.general.GeneralDTO;
 import com.igsl.configmigration.group.GroupDTO;
+import com.opensymphony.module.propertyset.PropertySet;
 
 @JsonDeserialize(using = JsonDeserializer.None.class)
 public class ApplicationUserUtil extends JiraConfigUtil {
@@ -33,7 +41,9 @@ public class ApplicationUserUtil extends JiraConfigUtil {
 	private static UserManager MANAGER = ComponentAccessor.getUserManager();
 	private static UserSearchService SEARCH_SERVICE = ComponentAccessor.getUserSearchService();
 	private static UserService SERVICE = ComponentAccessor.getComponent(UserService.class);
-	private static UserUtil UTIL = ComponentAccessor.getUserUtil();
+	private static final GroupManager GROUP_MANAGER = ComponentAccessor.getGroupManager();
+	private static final GroupService GROUP_SERVICE = ComponentAccessor.getComponent(GroupService.class);
+	private static final UserPropertyManager PROPERTY_MANAGER = ComponentAccessor.getUserPropertyManager();	
 	
 	@Override
 	public String getName() {
@@ -104,19 +114,16 @@ public class ApplicationUserUtil extends JiraConfigUtil {
 			original = (ApplicationUserDTO) findByDTO(newItem);
 		}
 		ApplicationUserDTO src = (ApplicationUserDTO) newItem;
+		ApplicationUser createdJira = null;
 		if (original != null) {
 			// Update user
-			ApplicationUser newUser = SERVICE.newUserBuilder((ApplicationUser) original.getJiraObject())
+			createdJira = SERVICE.newUserBuilder((ApplicationUser) original.getJiraObject())
 				.displayName(src.getDisplayName())
 				.emailAddress(src.getEmailAddress())
 				.build();
-			UpdateUserValidationResult uvr = SERVICE.validateUpdateUser(newUser);
+			UpdateUserValidationResult uvr = SERVICE.validateUpdateUser(createdJira);
 			if (uvr.isValid()) {
 				SERVICE.updateUser(uvr);
-				ApplicationUser createdJira = newUser;
-				ApplicationUserDTO created = new ApplicationUserDTO();
-				created.setJiraObject(createdJira);
-				result.setNewDTO(created);
 			} else {
 				StringBuilder sb = new StringBuilder();
 				for (String s : uvr.getErrorCollection().getErrorMessages()) {
@@ -131,10 +138,7 @@ public class ApplicationUserUtil extends JiraConfigUtil {
 					getAdminUser(), src.getName(), null, src.getEmailAddress(), src.getDisplayName());
 			CreateUserValidationResult cvr = SERVICE.validateCreateUser(cur);
 			if (cvr.isValid()) {
-				ApplicationUser createdJira = SERVICE.createUser(cvr);
-				ApplicationUserDTO created = new ApplicationUserDTO();
-				created.setJiraObject(createdJira);
-				result.setNewDTO(created);
+				createdJira = SERVICE.createUser(cvr);
 			} else {
 				StringBuilder sb = new StringBuilder();
 				for (String s : cvr.getErrorCollection().getErrorMessages()) {
@@ -143,13 +147,74 @@ public class ApplicationUserUtil extends JiraConfigUtil {
 				throw new Exception("Failed to create user: " + sb.toString());
 			}
 		}
+		// Update property set
+		PropertySet ps = PROPERTY_MANAGER.getPropertySet(createdJira);
+		ps.remove();	// Is it too risky to wipe them all out?
+		for (Map.Entry<String, GeneralDTO> entry : src.getProperties().entrySet()) {
+			GeneralDTO dto = entry.getValue();
+			ps.setAsActualType(entry.getKey(), dto.getValue());
+		}
+		// Update group membership
+		List<String> removeGroups = new ArrayList<>();
+		if (original != null) {
+			for (GroupDTO dto : original.getGroups()) {
+				removeGroups.add(dto.getName());
+			}
+		}
+		for (GroupDTO dto : src.getGroups()) {
+			removeGroups.remove(dto.getName());
+		}
+		List<String> addGroups = new ArrayList<>();
+		for (GroupDTO dto : src.getGroups()) {
+			addGroups.add(dto.getName());
+		}
+		if (original != null) {
+			for (GroupDTO dto : original.getGroups()) {
+				addGroups.remove(dto.getName());
+			}
+		}
+		JiraServiceContext ctx = new JiraServiceContextImpl(getAdminUser());
+		GroupRemoveChildMapper removeMapper = new GroupRemoveChildMapper(removeGroups);
+		removeMapper.register(src.getName());
+		boolean removeResult = GROUP_SERVICE.removeUsersFromGroups(ctx, removeMapper);
+		if (!removeResult) {
+			StringBuilder sb = new StringBuilder();
+			for (String s : removeGroups) {
+				sb.append(s).append(", ");
+			}
+			String list = "";
+			if (sb.length() > 2) {
+				list = sb.substring(0, sb.length() - 2);
+			}
+			result.addWarning("Unable to remove group(s): " + list);
+		}
+		List<String> users = new ArrayList<>();
+		users.add(createdJira.getName());
+		boolean addResult = GROUP_SERVICE.addUsersToGroups(ctx, addGroups, users);
+		if (!addResult) {
+			StringBuilder sb = new StringBuilder();
+			for (String s : addGroups) {
+				sb.append(s).append(", ");
+			}
+			String list = "";
+			if (sb.length() > 2) {
+				list = sb.substring(0, sb.length() - 2);
+			}
+			result.addWarning("Unable to add group(s): " + list);
+		}		
+		// Reload user
+		createdJira = MANAGER.getUserByName(createdJira.getName());
+		// Set result
+		ApplicationUserDTO created = new ApplicationUserDTO();
+		created.setJiraObject(createdJira);
+		result.setNewDTO(created);
 		return result;
 	}
 
 	@Override
 	public Map<String, JiraConfigDTO> search(String filter, Object... params) throws Exception {
 		Map<String, JiraConfigDTO> result = new TreeMap<>();
-		int limit = 1000;
+		int limit = 100000;
 		LOGGER.debug("UserSearchService: " + SEARCH_SERVICE.getClass().getCanonicalName());
 		UserSearchParams sp = UserSearchParams.builder(limit)
 				.allowEmptyQuery(true)
